645da6da41821096da86df293a5c98eff2ea808b
Matthew Arnold <matthew.arnold@ndph.ox.ac.uk>
2017-03-28 17:59:40 +0100

    Updated projectFiles that includes date of last modified file

--- R/projectFiles.r
+++ R/projectFiles.r
@@ -1,4 +1,4 @@
-projectFiles <- function(start_dir="~", filetypes=c("r", "sas"), filename="tmp") {
+projectFiles <- function(start_dir="~", filetypes=c("r", "sas"), filename="tmp", sort_mtime=TRUE) {
 
 	# get a list of files in the current directory
 	# use recursive=TRUE to follow the directory structure
@@ -6,22 +6,23 @@ projectFiles <- function(start_dir="~", filetypes=c("r", "sas"), filename="tmp")
 
 	cat("searching...")
 	flush.console()
-	file_list <- list.files(path=start_dir, recursive=TRUE)
+	filetype_list <- lapply(filetypes, function(z) list.files(start_dir, pattern=paste0("\\.", z, "$"), ignore.case=TRUE, recursive=TRUE))
 	cat("done\n")
 	flush.console()
 	
 	cat("processing...")
-	flush.console()
-	get_extension <- function(ext, fnames) {
-		# use toupper to make case-insensitive
-		spl <- strsplit(toupper(fnames), ".", fixed=TRUE)
-		lengths <- sapply(spl, length)
-		extensions <- sapply(spl, tail, 1)
-		return(fnames[ifelse(lengths==1, FALSE, ifelse(extensions == toupper(ext), TRUE, FALSE))])
-	}
-	
-	filetype_list <- lapply(filetypes, get_extension, fnames=file_list)
 	filetype_list <- lapply(filetype_list, sort)
+	filetype_list_mtime <- lapply(filetype_list, function(z) sapply(z, function(y) as.character(file.info(paste0(start_dir, "/",y))$mtime)))
+
+	if (sort_mtime) {
+
+		mtime_order <- lapply(filetype_list_mtime, order, decreasing=TRUE)
+		filetype_list <- mapply(function(l, z) l[z], l=filetype_list, z=mtime_order, SIMPLIFY=FALSE)
+		filetype_list_mtime <- mapply(function(l, z) l[z], l=filetype_list_mtime, z=mtime_order, SIMPLIFY=FALSE)
+
+
+	}
+
 	cat("done\n")
 	flush.console()
 	
@@ -31,7 +32,7 @@ projectFiles <- function(start_dir="~", filetypes=c("r", "sas"), filename="tmp")
 	
 	for (i in 1:length(filetype_list)) {
 		html <- paste0(html, "<p>", filetypes[i], "<br>")
-		crows <- paste0("<a href=\"file:///", paste0(start_dir, "/", filetype_list[[i]]), "\">", filetype_list[[i]], "</a>")
+		crows <- paste0("<a href=\"file:///", paste0(start_dir, "/", filetype_list[[i]]), "\">", filetype_list[[i]], "</a>&nbsp&nbsp&nbsplast modified: ", filetype_list_mtime[[i]])
 		crows <- paste(crows, collapse="<br>")
 		html <- paste0(html, crows)
 	}
