da3ee4e84b4f050c99dce58be1a8350002864b30
Matthew Arnold <matthew.arnold@ndph.ox.ac.uk>
2017-04-20 09:19:21 +0100

    Allow adjustment up or down when correcting text for presence of g j p q y hanging below the line

--- R/adjustYForText.r
+++ R/adjustYForText.r
@@ -1,8 +1,8 @@
-adjustYForText <- function(Ys, labels) {
+adjustYForText <- function(Ys, labels, subtract=TRUE) {
 
 	dropped.letters <- c("g", "j", "p", "q", "y")
 	adjust.Ys <- unique(as.vector(unlist(sapply(dropped.letters, function(z) grep(z, labels, fixed=TRUE)))))
-	Ys[adjust.Ys] <- Ys[adjust.Ys] - (strheight(expression(g)) - strheight(expression(c)))/2
+	Ys[adjust.Ys] <- Ys[adjust.Ys] + ifelse(subtract,-1,1)*(strheight(expression(g)) - strheight(expression(c)))/2
 	return(Ys)
 
 }
