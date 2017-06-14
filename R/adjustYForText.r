f5167b9c681f16915b510380892259e99cdd5cfb
Matthew Arnold <matthew.arnold@ndph.ox.ac.uk>
2017-06-14 13:43:57 +0100

    Add more dropped characters to Y text adjustment in forest plots

--- R/adjustYForText.r
+++ R/adjustYForText.r
@@ -1,6 +1,6 @@
 adjustYForText <- function(Ys, labels, subtract=TRUE) {
 
-	dropped.letters <- c("g", "j", "p", "q", "y")
+	dropped.letters <- c("g", "j", "p", "q", "y", "(", ")")
 	adjust.Ys <- unique(as.vector(unlist(sapply(dropped.letters, function(z) grep(z, labels, fixed=TRUE)))))
 	Ys[adjust.Ys] <- Ys[adjust.Ys] + ifelse(subtract,-1,1)*(strheight(expression(g)) - strheight(expression(c)))/2
 	return(Ys)
