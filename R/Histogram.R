c0a01eae38d51a10edff6863b58d89bbb5b4aa29
Matthew Arnold <matthew.arnold@ndph.ox.ac.uk>
2017-07-24 09:31:48 +0100

    Various fixes to customise generated histograms (motivated by AF-WMH project).
    Primarily allows customisation of the axes

--- R/Histogram.R
+++ R/Histogram.R
@@ -1,4 +1,24 @@
-Histogram <- function(values, breaks=NULL, xlim=NULL, ylim=NULL, xregion, yregion, addNorm=TRUE, addLogNorm=TRUE, addQAxis=TRUE, addXAxis=TRUE, addYAxis=TRUE, addLegend=TRUE, xlab="", ylab="", LogData=FALSE) {
+Histogram <- function(values, 
+						breaks=NULL, 
+						xlim=NULL, 
+						ylim=NULL, 
+						xregion=par("usr")[1:2], 
+						yregion=par("usr")[3:4], 
+						addNorm=TRUE, 
+						addLogNorm=TRUE, 
+						addQAxis=TRUE, 
+						addXAxis=TRUE, 
+						addYAxis=TRUE, 
+						addLegend=TRUE, 
+						addQuantiles=TRUE,
+						xticks=NULL,
+						ypercent=FALSE,
+						yticks=NULL,
+						yticklabs=NULL,
+						xlab="", 
+						ylab="", 
+						LogData=FALSE) 
+{
 
 	clend <- par("lend")
 	par(lend=2)
@@ -41,7 +61,6 @@ Histogram <- function(values, breaks=NULL, xlim=NULL, ylim=NULL, xregion, yregio
 	
 	if (addQAxis) {
 	
-	
 		# add quantiles using a special axis
 		Qaxis.at <- quantile(values, c(0.01, 0.05, 0.5, 0.95, 0.99), na.rm=TRUE)
 		newxat <- rep(0,length(Qaxis.at))
@@ -49,38 +68,79 @@ Histogram <- function(values, breaks=NULL, xlim=NULL, ylim=NULL, xregion, yregio
 			newpt <- QMap(Qaxis.at[i], 0,xlim, ylim, xregion, yregion)
 			newxat[i] <- newpt[1]			
 		}
-		axis(side=1, at=newxat, labels=c("1%", "5%", "Median", "95%", "99%"), pos=yregion[1], font.axis=1, cex.axis=0.8, las=1, lend=1, tcl=-0.25, mgp=c(3,0.1,0))
-		quantilesx <- newxat
+		# restrict to drawing points that are inside the plotting region
+		keepx <- newxat >= xregion[1] & newxat <= xregion[2]
+		print(keepx)
+		axis(side=1, at=newxat[keepx], labels=c("1%", "5%", "Median", "95%", "99%")[keepx], pos=yregion[1], font.axis=1, cex.axis=0.8, las=1, lend=1, tcl=-0.25, mgp=c(3,0.1,0))
+		quantilesx <- newxat[keepx]
+		
+		
 	}
 	
 	if (addXAxis) {
-		
-		Xaxis.at <- pretty(xlim)
-		Xaxis.at <- Xaxis.at[Xaxis.at > min(xlim) & Xaxis.at < max(xlim)]
+	
+		if (is.null(xticks)) {		
+			Xaxis.at <- pretty(xlim)
+			Xaxis.at <- Xaxis.at[Xaxis.at > min(xlim) & Xaxis.at < max(xlim)]
+		} else {
+			Xaxis.at <- xticks
+		}
 		newxat <- rep(0,length(Xaxis.at))
 		for (i in 1:length(newxat)) {
 			newpt <- QMap(Xaxis.at[i], 0,xlim, ylim, xregion, yregion)
 			newxat[i] <- newpt[1]			
 		}
-		axis(side=1, at=newxat, labels=Xaxis.at, pos=yregion[1], font.axis=2, las=1, lend=1, tcl=-0.9)
-		# add the axis extensions
-		text(x=mean(xregion), y=yregion[1]-4*strheight("A"), labels=xlab, font=2, adj=c(0.5,1))
+		
+		# add a base line over the entire length of the xregion
+		lines(x=xregion, y=rep(yregion[1],2), lend=2)
+		
+		# add the axis with ticks
+		axis(side=1, at=newxat, labels=Xaxis.at, pos=yregion[1], font.axis=2, las=1, lend=1, tcl=ifelse(addQAxis,-0.9,-0.5), mgp=c(3,ifelse(addQAxis,1,0.6),0))
+		
+		# add the axis labels
+		text(x=mean(xregion), y=yregion[1]-ifelse(addQAxis,4,3.5)*strheight("A"), labels=xlab, font=2, adj=c(0.5,1))
 	}
 		
 		
 	if (addYAxis) {	
-		Yaxis.at <- pretty(ylim)
+	
+		if (ypercent) {
+			# need to work out the scaling factor from density to counts
+			nonzero_counts <- histfit$counts > 0
+			count_scaling <- (histfit$counts[nonzero_counts] / histfit$density[nonzero_counts])[1]
+			percent_scaling <- count_scaling / length(values) * 100
+
+			if (is.null(yticks)) {
+				yticks <- c(0,20,40,60,80)
+			}
+			yticklabs <- yticks
+			
+			Yaxis.at <- yticks / percent_scaling
+		
+		} else {
+			if (is.null(yticks)) {
+				Yaxis.at <- pretty(ylim)
+			} else {
+				Yaxis.at <- yticks
+			}
+			if (any(!is.null(yticklabs), !is.null(yticks))) {
+				yticklabs <- Yaxis.at
+			}
+		}
+		
 		newyat <- rep(0,length(Yaxis.at))
 		for (i in 1:length(newyat)) {
 			newpt <- QMap(0, Yaxis.at[i], xlim, ylim, xregion, yregion)
 			newyat[i] <- newpt[2]			
 		}
-		axis(side=2, at=newyat, labels=Yaxis.at, pos=xregion[1]-strheight("A"), font.axis=2, las=1, lend=1)
+		axis(side=2, at=newyat, labels=yticklabs, pos=xregion[1]-strwidth("A"), font.axis=2, las=1, lend=1)
 		text(y=mean(newyat), x=xregion[1]-strheight("A")-strwidth("0.00\t\t\t  ", font=2), labels=ylab, font=2, adj=c(0.5,1), srt=90)
-		
-		# add a dashed line for the median
-		sapply(quantilesx, function(z) lines(x=rep(z,2), y=c(min(newyat), max(newyat)), col="green3", lty="dashed", lend=2))
+			
+	}
 	
+	if (addQuantiles) {
+		# add a dashed line for default quantiles
+		sapply(quantilesx, function(z) lines(x=rep(z,2), y=c(min(newyat), max(newyat)), col="green3", lty="dashed", lend=2))
 	}
 	
 	if (all(addNorm, addLogNorm)) {
