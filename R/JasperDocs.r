23cdcf6a0907185d132826422aceec45abb5b7fe
Matthew Arnold <matthew.arnold@ndph.ox.ac.uk>
2017-10-06 12:16:33 +0100

    Correction...
    Revert "Revert "Documentation updates""
    
    This reverts commit 2d81365a4f7f454a0bc28d4375445d721e9a796a.

--- R/JasperDocs.r
+++ R/JasperDocs.r
@@ -1,5 +1,6 @@
 JasperDocs <- function() {
 
+	if (!exists(".jasper.home", envir=.GlobalEnv)) stop("Jasper documentation location not known.\nAre you using the Jasper R package?\nDocumentation for some functions can be found by prefixing ?\nto the function name")
 	loc <- paste(.jasper.home, "\\documentation\\index.html", sep="")
 	browseURL(loc)
 
