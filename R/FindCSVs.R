59c2cc517b2cf56cd40438235a1d72652cec285c
Matthew Arnold <matthew.arnold@ndph.ox.ac.uk>
2018-01-16 10:24:58 +0000

    Add option to FindCSVs to automatically capitalise the column names in CSVs when read in

--- R/FindCSVs.R
+++ R/FindCSVs.R
@@ -1,6 +1,6 @@
 
 
-FindCSVs <- function(folder=".", verbose=TRUE) {
+FindCSVs <- function(folder=".", verbose=TRUE, upcase.names=FALSE) {
 
 	curwd <- getwd()
 	setwd(folder)
@@ -16,6 +16,10 @@ FindCSVs <- function(folder=".", verbose=TRUE) {
 			csvname <- substr(i, 1, nchar(i)-4)
 			object.names <- c(object.names, csvname)
 			assign(csvname, read.csv(i, as.is=TRUE), envir=.GlobalEnv)
+			
+			if (upcase.names) {
+				eval(parse(text=paste0("names(", csvname, ") <- toupper(names(", csvname, "))")), envir=.GlobalEnv)
+			}
 		
 		}
 	
